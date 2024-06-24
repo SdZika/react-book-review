b10565167dbdada4806e689f08b03877b42e099a
SdZika <markozivkovic1982@gmail.com>
2024-06-24 14:08:11 +0200

    add backgroun and search icon

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,10 +1,15 @@
 import "./Navbar.styles.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
 
 export const Navbar = () => {
     return (
         <nav className="navStyles">
+            <h1>Book Club</h1>
+            <input type="text" id="searchInput" placeholder="Search for books..." required />
+            <button id="searchButton" className="searchButton"><FontAwesomeIcon icon={faSearch} /></button>
             <ul> 
-                <li><a href="#">Browse Books</a></li>
                 <li><a href="#">Home</a></li>
                 <li><a href="#">Saved Review</a></li>
                 <li><a href="#">Favorites</a></li>
